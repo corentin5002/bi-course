73c98af89cef9f2907cc44851d784d33b69b4e53
corentin5002 <csabier@orange.fr>
2024-11-28 12:51:08 +0100

    final exam push

--- pages/Carrefour_monitoring.py
+++ pages/Carrefour_monitoring.py
@@ -6,13 +6,16 @@ import folium
 from numpy.ma.core import maximum_fill_value
 import json
 import os
+
+from pygments.lexer import default
 # import folium for streamlit
 from streamlit_folium import folium_static
+from datetime import datetime
 
 from utilsBox import *
 
 stations_df = pd.read_csv('Infos_Stations.csv')
-price_df = pd.read_csv('Prix_2024_truncated.csv')
+price_df = read_price_csv()
 
 stations_df.rename(columns={'ID': 'id'}, inplace=True)
 
@@ -32,14 +35,19 @@ stations_df['Enseignes'] = stations_df['Enseignes'].str.lower()
 stations_df['Enseignes'] = stations_df['Enseignes'].apply(remove_accents)
 
 # Aggregate enseignes with similar names
-rename_enseigne(stations_df, 'access', 'taccess')
-rename_enseigne(stations_df, 'total', 'total energy')
-rename_enseigne(stations_df, 'carrefour', 'carrefour')
-rename_enseigne(stations_df, 'intermarch', 'intermarche')
-rename_enseigne(stations_df, 'huit', '8aHuit')
-rename_enseigne(stations_df, 'system', 'system u')
-rename_enseigne(stations_df, ' u', 'system u')
-rename_enseigne(stations_df, 'esso', 'esso')
+rename_brand(stations_df, 'access', 'taccess')
+rename_brand(stations_df, 'total', 'total energy')
+rename_brand(stations_df, 'carrefour', 'carrefour')
+rename_brand(stations_df, 'intermarch', 'intermarche')
+rename_brand(stations_df, 'huit', '8aHuit')
+rename_brand(stations_df, 'system', 'system u')
+rename_brand(stations_df, ' u', 'system u')
+rename_brand(stations_df, 'esso', 'esso')
+rename_brand(stations_df, 'indepen', 'independant')
+rename_brand(stations_df, 'autre', 'independant')
+
+# Remove the independant enseignes from the dataframe
+stations_df = stations_df[stations_df['Enseignes'] != 'independant']
 
 # endregion Prepare the data
 
@@ -59,43 +67,68 @@ selected_fuel = st.sidebar.selectbox(
 )
 
 nb_range_station = st.sidebar.number_input('Min stations\'s number for a brand', min_value=0, max_value=1000, value=100)
+
+search_radius = st.sidebar.select_slider('Search radius (km)', options=range(5, 51, 5), value=10)
 # endregion sidebar
 
-dates= pd.to_datetime(price_df["Date"], format="%Y-%m-%d")
+dates = pd.to_datetime(price_df["Date"], format="%Y-%m-%d")
+price_df['Date'] = pd.to_datetime(price_df['Date'], format="%Y-%m-%d")
+
+default_date_range = [datetime(2024, 4, 1), datetime(2024, 4, 30)]
 
 # Interface utilisateur pour sélectionner la date
-selected_date = st.sidebar.date_input("Monitored date", value=dates.max(), min_value=dates.min(), max_value=dates.max())
+
+selected_date_range = st.sidebar.date_input("Monitored date", value=default_date_range, min_value=dates.min(),
+                                            max_value=dates.max())
+st.sidebar.html('<strong>Advice</strong> : Select more than a might be long to load the first time')
+
+if len(selected_date_range) == 2:
+    selected_date_range = [pd.to_datetime(date) for date in selected_date_range]
+    # st.write(f'original price df size :{len(price_df)}')
+    price_df = price_df[
+        (price_df['Date'] >= selected_date_range[0]) &
+        (price_df['Date'] <= selected_date_range[1])
+        ]
+    # st.write(f'original price df size :{len(price_df)}')
+    string_range_date = f'{selected_date_range[0].strftime("%Y-%m-%d")} to {selected_date_range[1].strftime("%Y-%m-%d")}'
+
+else:
+    selected_date_range = pd.to_datetime(selected_date_range[0])
+    # st.write(f'original price df size :{len(price_df)}')
+    price_df = price_df[price_df['Date'] == selected_date_range]
+    # st.write(f'filtered price df size :{len(price_df)}')
+    string_range_date = f'{selected_date_range.strftime("%Y-%m-%d")}'
 
 
 total_nb_stations = len(stations_df)
 stations_df = filter_nb_stations_df(stations_df, nb_range_station, selected_fuel)
-
 
 st.title('Carrefour monitoring')
 
 c1_1, c1_2 = st.columns(2)
 
 c1_1.metric('Number of stations monitored',
-           f'{len(stations_df)} stations',
-           delta= - (total_nb_stations - len(stations_df)),
-           delta_color='inverse'
-           )
+            f'{len(stations_df)} stations',
+            delta=- (total_nb_stations - len(stations_df)),
+            delta_color='inverse'
+            )
 
-c1_2.html(f'Filtered by number of stations for each brand <br> (<strong>{nb_range_station}</strong> minimum)')
+c1_1.html(f'Filtered by number of stations for each brand <br> (<strong>{nb_range_station}</strong> minimum)')
 
+nearby_competitors_id = get_dict_nearby_competitors('carrefour', search_radius)
 
-st.header('KPIs', divider='blue')
+c1_2.metric('Number of competitors',
+            sum([x for x in number_competitor_per_target(nearby_competitors_id).values()])
+            )
+st.header(f'KPIs from {string_range_date}', divider='blue')
 
-nearby_competitors_id = get_dict_nearby_competitors('carrefour', 10)
-# st.write(number_competitor_per_target(nearby_competitors_id))
+# region Folium map
+id_coord_dict = get_station_coord_dict(stations_df)
 
-target_id = get_station_coord_dict(stations_df)
-competitor_coord_dict = get_station_coord_dict(stations_df)
-
-focus_target = list(target_id.keys())[1]
+focus_target = list(id_coord_dict.keys())[1]
 
 # Create a map centered on the first station
-focus_target_map = folium.Map(location=target_id[focus_target], zoom_start=11)
+focus_target_map = folium.Map(location=id_coord_dict[focus_target], zoom_start=11)
 
 icon_size = 30
 # Add focus target marker
@@ -105,15 +138,14 @@ focus_target_icon = folium.features.CustomIcon(
 )
 
 folium.Marker(
-    location=target_id[focus_target],
+    location=id_coord_dict[focus_target],
     popup=focus_target,
     icon=focus_target_icon
 ).add_to(focus_target_map)
 
-
 if len(nearby_competitors_id[focus_target]) > 0:
     for id in nearby_competitors_id[focus_target]:
-        coords = [float(x) for x in competitor_coord_dict[id]]
+        coords = [float(x) for x in id_coord_dict[id]]
         folium.Marker(
             location=coords,
             popup=id,
@@ -122,6 +154,133 @@ if len(nearby_competitors_id[focus_target]) > 0:
 else:
     print('No competitors found')
 
-
 # display map
-folium_static(focus_target_map)
+# TODO uncomment
+# folium_static(focus_target_map)
+
+# endregion Folium map
+
+# region Price evolution
+
+# Convert selected_date_range to pd datetime
+price_df = price_df[price_df['id'].isin(stations_df['id'])]
+
+# Get the mean price for each fuel per brand
+
+brand_stations_dict = get_brand_stations_dict(stations_df)
+
+brand_price_dict = get_brand_price_dict(price_df, brand_stations_dict, selected_fuel, selected_date_range)
+
+# st.write(len(brand_price_dict.keys()))
+#
+# for day in brand_price_dict:
+#     st.write(day, len(brand_price_dict[day]), [len(brand_price_dict[day][brand]) for brand in brand_price_dict[day]])
+
+# KPIS
+
+target_brand_name = 'carrefour'
+
+target_brand_mean_price = float(
+    str(f"{np.mean([brand_price_dict[day][target_brand_name]['mean'] for day in brand_price_dict]):.2f}"))
+st.metric(
+    f'{'carrefour'.capitalize()} price',
+    f'{target_brand_mean_price} €/L'
+)
+
+brand_list = list(brand_stations_dict.keys())
+kpi_columns = [st.columns(3) for _ in range(len(brand_list) // 3)]
+
+# remove target brand from the brand list
+brand_list.remove(target_brand_name)
+
+for brand in brand_list:
+    brand_dayly_mean_price = [brand_price_dict[day][brand]['mean'] for day in brand_price_dict]
+
+    delta = np.mean(brand_dayly_mean_price) - target_brand_mean_price
+
+    # get on which row the brand is
+    row = brand_list.index(brand) // 3
+    column = brand_list.index(brand) % 3
+
+    kpi_columns[row][column].metric(
+        f'{brand.capitalize()} price',
+        f'{np.mean(brand_dayly_mean_price):.2f} €/L',
+        delta=f'{delta:.2f} €/L',
+    )
+
+# region Graph
+
+brand_dayly_mean_price_df = get_brand_daily_mean_price_df(brand_price_dict)
+
+# Plot of the mean prices of selected fuel for each brand
+
+fig = px.line(brand_dayly_mean_price_df,
+              x='Date',
+              y='Price',
+              color='Brand'
+              )
+
+fig.update_traces(
+    line=dict(
+        width=4,
+        color='lime'
+    ),
+    selector=dict(
+        name=target_brand_name
+    )
+)
+
+st.subheader(
+    f'Price evolution from {string_range_date}',
+    divider='blue'
+)
+st.plotly_chart(fig)
+
+
+# endregion Graph
+
+# endregion Price evolution
+
+
+# region station monitoring
+
+target_df = pd.read_csv('data/carrefour.csv')
+target_df = target_df[target_df['Type'].isin(stations_types_selection)]
+target_df['id'] = target_df['id'].apply(str)
+
+competitor_df = pd.read_csv('data/competitor.csv')
+competitor_df = competitor_df[competitor_df['Type'].isin(stations_types_selection)]
+competitor_df['id'] = competitor_df['id'].apply(str)
+
+target_id_adress_dict = get_id_adress_dict(target_df)
+
+selected_stations = st.multiselect('Select the target station', options=list(target_id_adress_dict.keys()))
+
+center_france_coord = [46.603354, 1.888334]
+if len(selected_stations) > 0:
+    map = folium.Map(location=center_france_coord, zoom_start=6)
+    count = 0
+    for station in selected_stations:
+        station_id = target_id_adress_dict[station]
+
+        folium.Marker(
+            location=id_coord_dict[station_id],
+            icon=folium.Icon(color='red'),
+            popup=station
+        ).add_to(map)
+
+        for competitor_id in nearby_competitors_id[station_id]:
+            competitor_address = stations_df[stations_df['id'] == competitor_id]['Adresse'].values[0]
+            competitor_popup = f"<b>{stations_df[stations_df['id'] == competitor_id]['Enseignes'].values[0]} : </b><br>{stations_df[stations_df['id'] == competitor_id]['Adresse'].values[0]},{stations_df[stations_df['id'] == competitor_id]['Ville'].values[0]}",
+            folium.Marker(
+                location=id_coord_dict[competitor_id],
+                popup=competitor_popup,
+            ).add_to(map)
+
+            count += 1
+
+    folium_static(map)
+
+    st.metric(f'Competitors found', count)
+
+# region station monitoring
--- utilsBox.py
+++ utilsBox.py
@@ -6,6 +6,18 @@ import os
 import json
 
 
+# region Read csv file
+def read_price_csv():
+    df_1 = pd.read_csv('data/prix_2024_jan_jun.csv')
+    df_2 = pd.read_csv('data/prix_2024_jun_last.csv')
+    return pd.concat([df_1, df_2])
+
+
+# endregion Read csv file
+
+
+# region Actions on dataframes
+
 def filter_nb_stations_df(df, nb_stations, selected_fuel):
     # count the number of stations for each enseignes
     number_station_brand = df.groupby('Enseignes')['id'].unique().apply(len).sort_values(ascending=False)
@@ -15,18 +27,27 @@ def filter_nb_stations_df(df, nb_stations, selected_fuel):
     selected_brands = number_station_brand[number_station_brand > nb_stations].index.tolist()
     return df[df['Enseignes'].isin(selected_brands)]
 
+
 def nb_station_fuel(df, fuel):
     fuel_info = df.groupby('id')[fuel].sum()
     fuel_info = fuel_info[fuel_info > 0]
     return len(fuel_info)
 
 
-def rename_enseigne(df, search_term, new_name):
+def rename_brand(df, search_term, new_name):
+    """
+    Dumb function to manually rename brand names
+    :param df:
+    :param search_term:
+    :param new_name:
+    :return:
+    """
     df.loc[df['Enseignes'].str.contains(search_term), 'Enseignes'] = new_name
 
 
 def remove_accents(text):
     return unidecode(text)
+
 
 def get_quartiles(df, fuel):
     q1 = df[fuel].quantile(0.25)
@@ -36,10 +57,20 @@ def get_quartiles(df, fuel):
 
 
 def replace_extremes(df, fuel):
+    """
+    Replace the values of the fuel column that are below the first quartile by the first quartile
+    (Objectif: remove the abberrant values)
+    :param df:
+    :param fuel:
+    :return:
+    """
     q = get_quartiles(df, fuel)
+    df.loc[df[fuel] < q['q1'], fuel] = q['q1']
     df.loc[df[fuel] < q['q1'], fuel] = q['q1']
     df.loc[df[fuel] > q['q3'], fuel] = q['q3']
 
+
+# endregion Actions on dataframes
 
 def haversine(lat1, lon1, lat2, lon2):
     # Convert degres to radians
@@ -61,17 +92,34 @@ def haversine(lat1, lon1, lat2, lon2):
     distance = R * c
     return distance
 
+
 def within_radius(row, radius, center):
-    return haversine(center[0], center[1], row['Latitude']/10**5, row['Longitude']/10**5) <= radius
+    return haversine(center[0], center[1], row['Latitude'] / 10 ** 5, row['Longitude'] / 10 ** 5) <= radius
 
 
 def get_station_coord_dict(df):
+    """
+    Returns a dictionary with the id as key and the coordinates as value
+    :param df:
+    :return:
+    """
     return {
-        df.loc[id, 'id']: (df.loc[id, 'Latitude'], df.loc[id, 'Longitude'])
+        str(df.loc[id, 'id']): (df.loc[id, 'Latitude'], df.loc[id, 'Longitude'])
         for id in df.index
     }
 
+
 def get_list_nearby_competitors(id_target, target_dict, competitor_dict, search_radius):
+    """
+    Returns the list of competitors that are within the search radius of the target station
+
+    :param id_target:
+    :param target_dict:
+    :param competitor_dict:
+    :param search_radius:
+    :return:
+    """
+
     L_competitors = list()
 
     for id_competitor in competitor_dict:
@@ -87,9 +135,16 @@ def get_list_nearby_competitors(id_target, target_dict, competitor_dict, search_
 
     return L_competitors
 
-def get_dict_nearby_competitors(target_brand, search_radius=10):
 
-    json_file_path = f'competitors_stations_{target_brand}_{search_radius}_km.json'
+def get_dict_nearby_competitors(target_brand, search_radius=10):
+    """
+    Returns a dictionary with the target station id as key and the list of competitors within the search radius as value
+    { id_target: [id_competitor1, id_competitor2, ...] }
+    :param target_brand:
+    :param search_radius:
+    :return:
+    """
+    json_file_path = f'data/competitors_stations_{target_brand}_{search_radius}_km.json'
 
     target_brand_df = pd.read_csv('data/carrefour.csv')
     competitors_df = pd.read_csv('data/competitor.csv')
@@ -102,16 +157,141 @@ def get_dict_nearby_competitors(target_brand, search_radius=10):
             nearby_id_competitor_dict = json.load(f)
     else:
         nearby_id_competitor_dict = {
-            id: get_list_nearby_competitors(id, target_id, competitor_coord_dict)
+            id: get_list_nearby_competitors(id, target_id, competitor_coord_dict, search_radius)
             for id in target_id
         }
 
         with open(json_file_path, 'w') as f:
-            json.dump(nearby_id_competitor_dict, f)
+            try:
+                json.dump(nearby_id_competitor_dict, f)
+            except:
+                pass
 
     return nearby_id_competitor_dict
+
+
+def get_target_nearby_brand_dict(stations_dict, stations_df):
+    """
+    Returns a dictionary with the target station id as key and the list of competitors within the search radius as value
+
+    { id_target: { brand1: [id_competitor1, id_competitor2, ...], brand2: [id_competitor1, id_competitor2, ...], ... } }
+    :param stations_dict:
+    :param stations_df:
+    :return:
+    """
+
+    final_dict = {}
+    for local_target_id in stations_dict:
+        list_brand = stations_df[stations_df['id'].isin(stations_dict[local_target_id])]['Enseignes'].unique().tolist()
+        local_target_dict = {}
+
+        # Filter ids in nearby_competitor_dict that got the brand
+        for brand in list_brand:
+            local_target_dict[brand] = \
+            stations_df[(stations_df['id'].isin(stations_dict[local_target_id])) & (stations_df['Enseignes'] == brand)][
+                'id'].tolist()
+
+        final_dict[local_target_id] = local_target_dict
+
+    return final_dict
+
 
 def number_competitor_per_target(dict):
     return {id: len(dict[id]) for id in dict}
 
+
+def get_brand_stations_dict(df):
+    """
+    Returns a dictionary with the brand as key and the list of stations as value
+
+    { brand1: [id_station1, id_station2, ...], brand2: [id_station1, id_station2, ...], ... }
+    :param df:
+    :return:
+    """
+    list_brands = df['Enseignes'].unique().tolist()
+    return {
+        brand: df[df['Enseignes'] == brand]['id'].tolist()
+        for brand in list_brands
+    }
+
+
+def get_brand_price_dict(price_df, brand_stations_dict, selected_fuel, selected_date_range):
+    """
+    Returns a dict with the price of the selected fuel for each station of each brand for each day in the selected date range
+
+    { day1: {
+        brand1: {
+            fuel: [station_price1, station_price2, ...],
+            mean: mean_price
+        },
+        ...
+    },
+    :param df:
+    :param brand:
+    :return:
+    """
+    if type(selected_date_range) == list:
+        date_string = f"{pd.to_datetime(selected_date_range[0]).strftime('%Y-%m-%d')}_{pd.to_datetime(selected_date_range[1]).strftime('%Y-%m-%d')}"
+        selected_date_range = pd.date_range(selected_date_range[0], selected_date_range[1])
+    else:
+        date_string = f'{pd.to_datetime(selected_date_range).strftime('%Y-%m-%d')}'
+        selected_date_range = pd.date_range(selected_date_range, selected_date_range)
+
+    path_file = f"data/brand_price_dict_{selected_fuel}_{date_string}.json"
+    if os.path.exists(path_file):
+        with open(path_file, 'r') as f:
+            brand_price_dict = json.load(f)
+    else:
+        # Create the list of days within the selected date range
+        brand_price_dict = {
+            day.strftime('%Y-%m-%d'): {
+                brand: {
+                    selected_fuel: price_df[
+                        (price_df['Date'] >= day) &
+                        (price_df['id'].isin(brand_stations_dict[brand]))
+                        ][selected_fuel].to_list(),
+
+                    'mean': price_df[
+                        (price_df['Date'] >= day) &
+                        (price_df['id'].isin(brand_stations_dict[brand]))
+                        ][selected_fuel].mean()
+                }
+                for brand in brand_stations_dict
+            }
+            for day in selected_date_range
+        }
+
+        with open(path_file, 'w') as f:
+            json.dump(brand_price_dict, f)
+
+    return brand_price_dict
+
+# Graph functions
+
+def get_brand_daily_mean_price_df(brand_price_dict):
+    """
+    Returns a DataFrame with columns ['Date', 'Brand', 'Price'] from a brand_price_dict {day: {brand: {mean: price}}}
+    :param brand_price_dict: Dictionary with the structure {day: {brand: {mean: price}}}
+    :return: pandas DataFrame
+    """
+    data = []
+
+    for day, brands in brand_price_dict.items():
+        for brand, details in brands.items():
+            data.append({
+                'Date': day,
+                'Brand': brand,
+                'Price': details['mean']
+            })
+
+    temp_df = pd.DataFrame(data, columns=['Date', 'Brand', 'Price'])
+
+    return temp_df
+
+
+def get_id_adress_dict(df):
+    return {
+        f'{row["Adresse"]}, {row["Ville"]}': row['id']
+        for _, row in df.iterrows()
+    }
 
